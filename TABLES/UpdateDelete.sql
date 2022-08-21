a7378866a60a954794ed10fbaee4319e78854423
Ali Osman Demirkollu <72684098+ali-osman-de@users.noreply.github.com>
2022-08-21 18:19:24 +0300

    Create UpdateDelete.sql

---
+++ TABLES/UpdateDelete.sql
@@ -0,0 +1,62 @@
+--https://www.mockaroo.com/
+-- randomly data creater website for sql insert into already datasets codes
+
+-- SELECT * FROM author;
+
+--ALL DATA UPDATE
+-- UPDATE <table_name>
+-- SET column1 = value1,
+-- 	column2 = value2,
+-- 	******;
+
+
+-- UPDATE <table_name>
+-- SET column1 = value1,
+-- 	   column2 = value2,
+-- 	   ******;
+-- WHERE condition;
+
+-- UPDATE author
+-- 	SET first_name = 'Emrah Safa',
+-- 		last_name = 'Gürkan',
+-- 		email = 'emrah@gurkan.com',
+-- 		birthday = '1980-01-01'
+-- WHERE id = 10;
+
+-- SELECT * FROM author;
+
+-- UPDATE author
+-- SET first_name = 'XXXX',
+-- 	last_name = 'YYYY'
+-- WHERE first_name LIKE 'V%';
+
+
+-- SELECT * FROM author;
+
+-- UPDATE author
+-- SET last_name = 'UPDATE'
+-- WHERE first_name = 'Sabahattin'
+-- RETURNING *;
+
+-- DELETE FROM <table_name>
+-- WHERE condition;
+
+-- DELETE FROM author 
+-- WHERE id = 6;
+
+-- SELECT * FROM author;
+
+-- DELETE FROM author
+-- WHERE id>11
+-- RETURNING *;
+
+
+
+
+
+
+
+
+
+
+
