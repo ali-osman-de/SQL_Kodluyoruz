b4bea2414aa982a217be50e8dc332decc9e5c63f
Ali Osman Demirkollu <72684098+ali-osman-de@users.noreply.github.com>
2022-08-26 15:53:57 +0300

    Create odev11.sql

---
+++ odev11.sql
@@ -0,0 +1,73 @@
+
+--1.Question Answer
+
+(
+SELECT first_name
+FROM actor
+)
+UNION
+(
+SELECT first_name
+FROM customer
+);
+
+
+--2.Question Answer
+
+(
+SELECT first_name
+FROM actor
+)
+INTERSECT
+(
+SELECT first_name
+FROM customer
+);
+
+--3.Question Answer
+
+(
+SELECT first_name
+FROM actor
+)
+EXCEPT
+(
+SELECT first_name
+FROM customer
+);
+
+--4.Question Answer
+
+(
+SELECT first_name
+FROM actor
+)
+UNION ALL
+(
+SELECT first_name
+FROM customer
+);
+
+
+(
+SELECT first_name
+FROM actor
+)
+INTERSECT ALL
+(
+SELECT first_name
+FROM customer
+);
+
+
+(
+SELECT first_name
+FROM actor
+)
+EXCEPT ALL
+(
+SELECT first_name
+FROM customer
+);
+
+
